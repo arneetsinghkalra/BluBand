367b3065e2625ef132b1c37242ee37da553848ca
arneetsinghkalra <a.s.kalra1999@gmail.com>
2020-03-25 19:30:46 -0400

    Minor updates

--- app/src/main/java/com/mcgill/bluband/MainActivity.java
+++ app/src/main/java/com/mcgill/bluband/MainActivity.java
@@ -45,8 +45,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
+                openLoginActivity();
                 finish();
             }
         },SPLASH_SCREEN);
