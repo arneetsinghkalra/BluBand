9b8c332d36a9f84a4e26568fc2bf62813701fa83
arneetsinghkalra <a.s.kalra1999@gmail.com>
2020-03-25 16:56:13 -0400

    Biodesign logo added splash screen

--- app/src/main/java/com/mcgill/bluband/MainActivity.java
+++ app/src/main/java/com/mcgill/bluband/MainActivity.java
@@ -19,24 +19,26 @@ public class MainActivity extends AppCompatActivity {
 
     private static int SPLASH_SCREEN = 1250;
     private TextView slogan;
-   private ImageView bluband, mcgill;
+   private ImageView bluband, mcgill, biodesign;
    private Animation topAnim, bottomAnim;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         slogan = (TextView)findViewById(R.id.sloganID);
         bluband = (ImageView)findViewById(R.id.bluBandLogo);
         mcgill = (ImageView)findViewById(R.id.mcgillID);
+        biodesign = (ImageView)findViewById(R.id.biodesignID);
         topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
         bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
 
         bluband.setAnimation(topAnim);
         slogan.setAnimation(bottomAnim);
+        mcgill.setAnimation(bottomAnim);
         mcgill.setAnimation(bottomAnim);
 
 
