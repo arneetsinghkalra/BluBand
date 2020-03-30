78fededa86176c6251f271ba552c67f259c6ffb1
annyhang <annyhang@users.noreply.github.com>
2020-03-30 16:40:32 -0400

    Create OldHomeActivity.java

---
+++ app/src/main/java/com/mcgill/bluband/OldHomeActivity.java
@@ -0,0 +1,62 @@
+package com.mcgill.bluband;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class OldHomeActivity extends AppCompatActivity {
+
+    private Button logoutButton;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oldhome);
+/*
+
+        //To Sign out
+        logoutButton = findViewById(R.id.logoutID);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                openLoginActivity(); //Go back to login page
+            }
+        });
+
+        //TODO
+        //To setup new user
+        Button setupButton = findViewById(R.id.setupBtn);
+        setupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewUserActivity();
+            }
+        });
+*/
+
+    }
+
+    public void openLoginActivity(){
+        Intent intent = new Intent(this,LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void openNewUserActivity(){
+        Intent intent = new Intent(OldHomeActivity.this, NewUserActivity.class);
+        startActivity(intent);
+    }
+
+}
