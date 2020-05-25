76fc3423da93b0942a1cbc64ee4ef34af8319d5c
arneetsinghkalra <a.s.kalra1999@gmail.com>
2020-05-25 18:41:58 -0400

    Added Nurses Class and layout

--- app/src/main/java/com/mcgill/bluband/NursesActivity.java
+++ app/src/main/java/com/mcgill/bluband/NursesActivity.java
@@ -1,35 +1,27 @@
 package com.mcgill.bluband;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.widget.Toolbar;
-
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import androidx.appcompat.widget.Toolbar;
+
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.mikepenz.materialdrawer.AccountHeaderBuilder;
 import com.mikepenz.materialdrawer.Drawer;
 import com.mikepenz.materialdrawer.DrawerBuilder;
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 
-import java.util.ArrayList;
-
-public class HomeActivity extends BaseActivity {
+public class NursesActivity extends BaseActivity {
     private ListView listView;
     private DatabaseReference myDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_nurses);
 
         //**********************Create Nav Bar with custom Tool Bar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -42,7 +34,7 @@ public class HomeActivity extends BaseActivity {
             }
         });
 
-        navHeader  = new AccountHeaderBuilder()
+        navHeader = new AccountHeaderBuilder()
                 .withActivity(this)
                 .withTranslucentStatusBar(true)
                 .withHeaderBackground(R.mipmap.logo)
@@ -57,7 +49,7 @@ public class HomeActivity extends BaseActivity {
                 .withActivity(this)
                 .withAccountHeader(navHeader)
                 .withActionBarDrawerToggle(true)
-                .withSelectedItem(1)
+                .withSelectedItem(3)
                 .addDrawerItems(
                         home,
                         children,
@@ -67,17 +59,17 @@ public class HomeActivity extends BaseActivity {
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
-                        switch(position){
+                        switch (position) {
                             //Home Button
                             case 1:
-                                reOpenCurrentActivity();
+                                openHomeActivity();
                                 break;
                             //Children
                             case 2:
                                 openNewChildActivity();
                                 break;
                             case 3:
-                                openNursesActivity();
+                                reOpenCurrentActivity();
                                 break;
                             //Logout case
                             case 4:
@@ -88,31 +80,5 @@ public class HomeActivity extends BaseActivity {
                     }
                 })
                 .build();
-        //********************** End of nav bar code
-
-        //Firebase database -------------------
-        listView = (ListView) findViewById(R.id.dashboard);
-        final ArrayList<String> list = new ArrayList<>();
-        final ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, list);
-        listView.setAdapter(adapter);
-
-        myDatabase = FirebaseDatabase.getInstance().getReference().child("children");
-        myDatabase.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                list.clear();
-                for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
-                    Child aChild = snapshot.getValue(Child.class);
-                    String txt = aChild.getName()+": "+aChild.getGlucose() ;
-                    list.add(txt);
-                }
-                adapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
     }
 }
